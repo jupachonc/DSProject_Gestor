c510446c2f16fecb408cfec1e16f84f1fda6294e
sebastian <jupachonc@unal.edu.co>
2020-04-15 21:25:51 -0500

    Lista Implementation

--- app/src/main/java/co/dsproject/gestor/Lista.java
+++ app/src/main/java/co/dsproject/gestor/Lista.java
@@ -1,5 +1,60 @@
 package co.dsproject.gestor;
 
-public class Lista {
+import org.w3c.dom.Node;
+
+public class Lista<T extends Comparable<T>> {
+
+    private NodeGeneric<T> head;
+
+    public Lista(){
+        head = null;
+    }
+
+    public boolean insert(T item){
+
+        boolean inserted;
+        NodeGeneric<T> ptr, prev;
+        inserted = false;
+        ptr = head;
+        prev = null;
+        while(ptr != null && ptr.getData().compareTo(item) < 0){
+            prev = ptr;
+            ptr = ptr.getNext();
+        }
+
+        if(ptr == null && !(ptr.getData().equals(item))){
+            inserted = true;
+            NodeGeneric<T> newNode = new NodeGeneric();
+            newNode.setData(item);
+            newNode.setNext(ptr);
+            if(prev == null)
+                head = newNode;
+            else
+                prev.setNext(newNode);
+        }
+        return  inserted;
+    }
+
+    public boolean delete(T item){
+
+        boolean deleted;
+        NodeGeneric<T> ptr, prev;
+        deleted = false;
+        ptr = head;
+        prev = null;
+
+        while(ptr != null && !ptr.getData().equals(item)){
+            prev = ptr;
+            ptr = ptr.getNext();
+        }
+        if(ptr != null && ptr.getData().equals(item)){
+            deleted = true;
+            prev.setNext(ptr.getNext());
+        }
+        return deleted;
+    }
+
+
+
 
 }
