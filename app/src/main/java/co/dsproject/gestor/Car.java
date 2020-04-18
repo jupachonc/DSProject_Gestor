a063e5d7e6640c17a9094eccecac713f60a87d19
sebastian <jupachonc@unal.edu.co>
2020-04-18 12:17:04 -0500

    Cars Fragment

--- app/src/main/java/co/dsproject/gestor/Car.java
+++ app/src/main/java/co/dsproject/gestor/Car.java
@@ -1,5 +1,6 @@
 package co.dsproject.gestor;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 public class Car implements Comparable<Car> {
@@ -9,15 +10,15 @@ public class Car implements Comparable<Car> {
     private String marca;
     private String linea;
     private int modelo;
-    private Date ulltimo_mantenimiento;
+    private LocalDate ulltimo_mantenimiento;
     private int frecuencia_mantenimiento;
-    private Date soat;
-    private Date rtm;
-    private Date poliza;
-    private Date impuesto;
+    private LocalDate soat;
+    private LocalDate rtm;
+    private LocalDate poliza;
+    private LocalDate impuesto;
 
-    public Car(String owner, String placa, String marca, String linea, int modelo, Date ulltimo_mantenimiento,
-        int frecuencia_mantenimiento, Date soat, Date rtm, Date poliza, Date impuesto){
+    public Car(String owner, String placa, String marca, String linea, int modelo, LocalDate ulltimo_mantenimiento,
+        int frecuencia_mantenimiento, LocalDate soat, LocalDate rtm, LocalDate poliza, LocalDate impuesto){
         this.owner = owner;
         this.placa = placa;
         this.marca = marca;
@@ -35,5 +36,93 @@ public class Car implements Comparable<Car> {
     @Override
     public int compareTo(Car o) {
         return this.placa.compareTo(o.placa);
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getLinea() {
+        return linea;
+    }
+
+    public void setLinea(String linea) {
+        this.linea = linea;
+    }
+
+    public int getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(int modelo) {
+        this.modelo = modelo;
+    }
+
+    public LocalDate getUlltimo_mantenimiento() {
+        return ulltimo_mantenimiento;
+    }
+
+    public void setUlltimo_mantenimiento(LocalDate ulltimo_mantenimiento) {
+        this.ulltimo_mantenimiento = ulltimo_mantenimiento;
+    }
+
+    public int getFrecuencia_mantenimiento() {
+        return frecuencia_mantenimiento;
+    }
+
+    public void setFrecuencia_mantenimiento(int frecuencia_mantenimiento) {
+        this.frecuencia_mantenimiento = frecuencia_mantenimiento;
+    }
+
+    public LocalDate getSoat() {
+        return soat;
+    }
+
+    public void setSoat(LocalDate soat) {
+        this.soat = soat;
+    }
+
+    public LocalDate getRtm() {
+        return rtm;
+    }
+
+    public void setRtm(LocalDate rtm) {
+        this.rtm = rtm;
+    }
+
+    public LocalDate getPoliza() {
+        return poliza;
+    }
+
+    public void setPoliza(LocalDate poliza) {
+        this.poliza = poliza;
+    }
+
+    public LocalDate getImpuesto() {
+        return impuesto;
+    }
+
+    public void setImpuesto(LocalDate impuesto) {
+        this.impuesto = impuesto;
     }
 }
--- app/src/main/java/co/dsproject/gestor/ui/cars/CarsViewModel.kt
+++ app/src/main/java/co/dsproject/gestor/ui/cars/CarsViewModel.kt
@@ -1,7 +1,66 @@
 package co.dsproject.gestor.ui.cars
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import co.dsproject.gestor.Car
+import co.dsproject.gestor.Lista
+import java.time.LocalDate
+import java.time.Month
+import java.util.*
 
 class CarsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private var _Cars = Lista<Car>()
+    private var head = _Cars.head
+
+    private val _owner = MutableLiveData<String>().apply { value = head.data.owner }
+    private val _placa = MutableLiveData<String>().apply { value = head.data.placa }
+    private val _marca = MutableLiveData<String>().apply { value = head.data.marca }
+    private val _linea = MutableLiveData<String>().apply { value = head.data.linea }
+    private val _modelo = MutableLiveData<Int>().apply { value = head.data.modelo }
+    private val _ulltimo_mantenimiento = MutableLiveData<LocalDate>().apply { value = head.data.ulltimo_mantenimiento }
+    private val _frecuencia_mantenimiento = MutableLiveData<Int>().apply { value = head.data.frecuencia_mantenimiento }
+    private val _soat = MutableLiveData<LocalDate>().apply { value = head.data.soat }
+    private val _rtm = MutableLiveData<LocalDate>().apply { value = head.data.rtm }
+    private val _poliza = MutableLiveData<LocalDate>().apply { value = head.data.poliza }
+    private val _impuesto = MutableLiveData<LocalDate>().apply { value = head.data.impuesto }
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "Este es el fragmento car"
+    }
+
+    val text : LiveData<String> = _text
+
+    val owner : LiveData<String> = _owner
+    val placa : LiveData<String> = _placa
+    val marca : LiveData<String> = _marca
+    val linea : LiveData<String> = _linea
+    val modelo : LiveData<Int> = _modelo
+    val ultimomantenimiento : LiveData<LocalDate> = _ulltimo_mantenimiento
+    val frecuenciamantenimiento : LiveData<Int> = _frecuencia_mantenimiento
+    val soat : LiveData<LocalDate> = _soat
+    val rtm : LiveData<LocalDate> = _rtm
+    val poliza : LiveData<LocalDate> = _poliza
+    val impuesto : LiveData<LocalDate> = _impuesto
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun car(){
+        val carro = Car(
+                "ME",
+                "ABC123",
+                "CHEVROLET",
+                "GT",
+                2009,
+                LocalDate.of(2020, Month.JANUARY, 1),
+                15,
+                LocalDate.of(2020, Month.JANUARY, 1),
+                LocalDate.of(2020, Month.JANUARY, 1),
+                LocalDate.of(2020, Month.JANUARY, 1),
+                LocalDate.of(2020, Month.JANUARY, 1)
+
+        )
+        _Cars.insert(carro)
+    }
 }
