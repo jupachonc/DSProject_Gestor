b1e7a76856036c0d9ac1d3ecdd9278ef82ac8f05
sebastian <jupachonc@unal.edu.co>
2020-04-17 22:33:10 -0500

    Cars Fragment

--- app/src/main/java/co/dsproject/gestor/ui/home/HomeViewModel.kt
+++ app/src/main/java/co/dsproject/gestor/ui/home/HomeViewModel.kt
@@ -3,8 +3,12 @@ package co.dsproject.gestor.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import co.dsproject.gestor.Car
+import co.dsproject.gestor.Lista
 
 class HomeViewModel : ViewModel() {
+
+    private var cars_list = Lista<Car>()
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
